4c21257ebdf3237d9501c1cf180b6ff042f87d3f
Oleg <110766679+oleg4586@users.noreply.github.com>
2022-10-24 10:43:27 +0400

    Add files via upload

---
+++ LessonSeminarWork/JS/sample.js
@@ -0,0 +1,27 @@
+// Task_1
+// alert('Hello, dear frends!');
+
+// Task_4
+// let first_name = 'Sam';
+// function showMessage() {
+//     let message = 'Hello, ' + first_name;
+//     alert(message);
+// }
+
+// showMessage();
+
+//Task_5
+// u_q = confirm('Are you sure?')
+
+// let age = parseInt(prompt('Add your age1'));
+
+// if (age == 18) {
+//     alert('Вы совершеннолетний, все можно!');
+// } else if (age == 10) {
+//     alert('Вам надо учить уроки!');
+// } else if (age == 30) {
+//     alert('Ложитесь спать, завтра на работу');
+// } else {
+//     alert('Мы не знаем что Вам делать');
+// }
+
